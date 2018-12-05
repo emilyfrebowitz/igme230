7e7dc0a86fe17df21870807624e6eb38fb1d551c
Emily Frebowitz <erf1545@rit.edu>
2018-12-04 22:59:58 -0500

    practical sample

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,1 +1,26 @@
-/* Don't use <script> tags in a linked js file! */
+/*AJAX CONTENT LOADING*/
+
+let selection = ("content1.txt") // sets default content to be displayed
+$("#choose-content").val(selection); // changes menu option to the default
+$("#content").load(selection); //retrieves only the default element
+
+/*when the content selection is changed
+change the value of the selection and load that content*/
+
+$("#choose-content").change(function() {
+    selection = $(this).val();
+    $("#content").load(selection);
+});
+
+
+/* CONTENT MENU FUNCTION
+when a menubox is clicked, finds the submenu
+for the selected and toggles it's display on and off*/
+$(document).ready(function() {
+    $(".submenu").hide()
+});
+
+$(".menubox").click(function() {
+    $(this).find(".submenu").slideToggle();
+    /*slideToggle() - toggle function w slide animation*/
+})
