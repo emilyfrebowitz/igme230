542356b19492be977872e8e9cf823eb6ec889650
Emily Frebowitz <erf1545@rit.edu>
2018-12-13 12:17:04 -0500

    test 12

--- makeup4/fall2018finalmakeup.js
+++ makeup4/fall2018finalmakeup.js
@@ -11,7 +11,7 @@ $("#stories").val(selection); // changes menu option to the default
 $("#story").load(selection); //retrieves only the default element
 
 $("#stories").change(function() {
-    selection = $(this).attr("li[id]");
+    selection = $(this).attr("id");
     $("#story").load(selection);
 });
 
