75bfd7b5109be02f056e9ecebbca20b4311f9ed6
Emily Frebowitz <erf1545@rit.edu>
2018-12-13 11:32:21 -0500

    test 1

---
+++ makeup4/fall2018finalmakeup.js
@@ -0,0 +1,11 @@
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+
+/*naav menu toggle*/
+$("nav div").click(function() {
+    $(this).find(".submenu").slideToggle();
+});
+
+/*content loading*/
+let selection = ("story0.txt") // sets default content to be displayed
+$("#stories").val(selection); // changes menu option to the default
+$("#story").load(selection); //retrieves only the default element
