1ef9e7681eb01e3fcb9fc747779770c55ab391c0
Emily Frebowitz <erf1545@rit.edu>
2018-12-13 11:52:44 -0500

    test 5

--- makeup4/fall2018finalmakeup.js
+++ makeup4/fall2018finalmakeup.js
@@ -7,8 +7,17 @@ $("nav div").click(function() {
 
 /*content loading*/
 let selection = ("story0.txt") // sets default content to be displayed
-$("#stories li").on('click', function () {
+$("#stories li").click(function () {
     $(this).id;
 });
 $("#stories").val(selection); // changes menu option to the default
 $("#story").load(selection); //retrieves only the default element
+
+
+/*click counter*/
+var clicks = 0;
+$("#currentcount").text(clicks); //turns integer into string and displays
+$("#increment").click(function(){
+    clicks++;
+    $("#currentcount").text(clicks);
+});
